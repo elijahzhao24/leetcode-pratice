632ff4609a7b4cd963c5652b5960ef7f4e9fc857
Elijah Zhao <126823117+elijahzhao24@users.noreply.github.com>
2025-07-23 13:04:24 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int storage[26] = {0};
+        for (char c : tasks) {
+            storage[c - 'A']++;
+        }
+
+        priority_queue<int> pq;
+        for (int i:storage) {
+            if (i > 0) {
+                pq.push(i);
+            }
+        }
+
+        int steps = 0;
+        while (!pq.empty()) {
+            int cycle = n + 1;
+            vector<int> temp;
+
+            while (cycle-- > 0 && !pq.empty()) {
+                int cnt = pq.top(); 
+                pq.pop();
+                if (cnt - 1 > 0)
+                    temp.push_back(cnt - 1);
+                steps++;
+            }
+
+            for (int i :temp) {
+                pq.push(i);
+            }
+
+            if (pq.empty()) {
+                return steps;
+            } else {
+                steps += cycle;
+            }
+
+        }
+        return steps;
+    }
+};
