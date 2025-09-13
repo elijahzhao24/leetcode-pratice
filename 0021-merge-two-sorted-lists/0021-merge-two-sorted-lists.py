3a489a3a798964a4ddf9b49fe444fa9b0c68fce5
Elijah Zhao <126823117+elijahzhao24@users.noreply.github.com>
2025-09-12 23:52:19 -0600

    Time: 0 ms (100%), Space: 17.7 MB (61.46%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = None
+        l1 = list1
+        l2 = list2
+
+        # initialize head with the smaller first element
+        if l1:
+            if l2:
+                if l1.val <= l2.val:           # <-- choose smaller (<=)
+                    head = ListNode(l1.val)
+                    l1 = l1.next
+                else:
+                    head = ListNode(l2.val)
+                    l2 = l2.next
+            else:
+                head = ListNode(l1.val)
+                l1 = l1.next
+        else:
+            if l2:
+                head = ListNode(l2.val)
+                l2 = l2.next
+
+        cur = head
+
+        while l1 or l2:
+            if l1:
+                if l2:
+                    if l1.val <= l2.val:       # <-- choose smaller (<=)
+                        cur.next = ListNode(l1.val)
+                        l1 = l1.next
+                    else:
+                        cur.next = ListNode(l2.val)
+                        l2 = l2.next
+                else:
+                    cur.next = ListNode(l1.val)
+                    l1 = l1.next
+            else:
+                if l2:
+                    cur.next = ListNode(l2.val)
+                    l2 = l2.next
+            cur = cur.next
+        return head
