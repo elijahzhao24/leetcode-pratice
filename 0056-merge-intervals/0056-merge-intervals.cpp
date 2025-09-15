5239ca1d8ee7fbed252502fd4b5539dc7bb9052b
Elijah Zhao <126823117+elijahzhao24@users.noreply.github.com>
2025-09-15 17:27:15 -0600

    Time: 131 ms (5.18%), Space: 52.7 MB (5.04%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return {};
-
-        sort(intervals.begin(), intervals.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 return a[0] < b[0]; // ascending by start
-             });
 
         vector<vector<int>> ans;
+        if (intervals.empty()) {
+            return ans;
+        }
+
+        sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b) {
+            return a[0] < b[0];
+        });
+
         ans.push_back(intervals[0]);
 
         for (size_t i = 1; i < intervals.size(); ++i) {
@@ -22,4 +24,5 @@ public:
         }
         return ans;
     }
+        
 };
