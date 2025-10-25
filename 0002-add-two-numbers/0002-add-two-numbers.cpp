af54d72042559cb210639bc61332fba579abbccb
Elijah Zhao <126823117+elijahzhao24@users.noreply.github.com>
2025-10-24 20:49:25 -0700

    Time: 0 ms (100%), Space: 76.5 MB (98.15%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -8,36 +8,62 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode* curl1 = l1;
+        ListNode* curl2 = l2;
+        int lenl1 = 0;
+        int lenl2 = 0;
 
-        ListNode dummy(0);
-        ListNode* tail = &dummy;
+        while (curl1) {
+            ++lenl1;
+            curl1 = curl1->next;
+        }
+        while (curl2) {
+            ++lenl2;
+            curl2 = curl2->next;
+        }
+
+        ListNode* returnHead;
+        ListNode* lcur;
+        ListNode* scur;
         int carry = 0;
-
-        while (l1 || l2) {
-            if (l1) {
-                carry += l1->val;
-                l1 = l1->next;
-            }
-            if (l2) {
-                carry += l2->val;
-                l2 = l2->next;
-            }
-
-            // ListNode temp(carry%10);
-            tail->next = new ListNode(carry%10);
-            tail = tail->next;
-            carry = carry/10;
+        
+        if (lenl1>=lenl2) {
+            returnHead = l1;
+            lcur = l1;
+            scur = l2;
+        } else {
+            returnHead = l2;
+            lcur = l2;
+            scur = l1;
         }
 
-        if (carry) {
-            tail->next = new ListNode(carry%10);
-            tail = tail->next;
+        while (lcur) {
+            int curtotal = carry;
+            curtotal += lcur->val;
+            if (scur) {
+                curtotal += scur->val;
+                scur = scur->next;
+            }
+            carry = curtotal/10;
+            lcur->val = curtotal % 10;
+            lcur= lcur->next;
         }
 
-        return dummy.next;
+        if (carry > 0) {
+            lcur = returnHead;
+            while (lcur->next) {
+                lcur = lcur->next;
+            }
+            lcur->next = new ListNode(carry);
+        }
+
+
+        return returnHead;
+        
     }
-
 };
